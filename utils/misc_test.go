2c8c35e5124185e0cd27001c00577d1db4ee3b77
Sajal Kayan <sajal@turbobytes.com>
2016-10-21 14:53:46 +0700

    example code to bypass security checks, to be used while testing curl tests

--- utils/misc_test.go
+++ utils/misc_test.go
@@ -26,3 +26,31 @@ func TestLocalIP(t *testing.T) {
 		}
 	}
 }
+
+//TestOverrideSecurity demonstrates how to override security checks for testing purposes
+func TestOverrideSecurity(t *testing.T) {
+	originallocalipv4 := localipv4
+	localipv4 = []string{}
+	originallocalipv6 := localipv6
+	localipv6 = []string{}
+	//None of these should be security issue
+	cases_private := []string{"127.0.0.1", "10.5.6.4", "192.168.5.99", "100.66.55.66", "fd07:a47c:3742:823e:3b02:76:982b:463", "::1"}
+	for _, ipstr := range cases_private {
+		ip := net.ParseIP(ipstr)
+		v := islocalip(ip)
+		if v {
+			t.Errorf("Should be false for %s because we blanked it", ipstr)
+		}
+	}
+	//Restore original behaviour
+	localipv4 = originallocalipv4
+	localipv6 = originallocalipv6
+	//Test again to see if we could restore it
+	for _, ipstr := range cases_private {
+		ip := net.ParseIP(ipstr)
+		v := islocalip(ip)
+		if !v {
+			t.Error("Should be true for " + ipstr)
+		}
+	}
+}
